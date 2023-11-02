6a06cbb0f20e4bd04d2ae67e574ef5308438138d
Miroslav <77319935+NitroDaxs@users.noreply.github.com>
2023-11-02 12:36:43 +0200

    Add files via upload

---
+++ EntityFramework/Entity Relations/01.StudentSystem/P01_StudentSystem/Data/Models/Config/ConnectionString.cs
@@ -0,0 +1,8 @@
+﻿namespace P01_StudentSystem.Data.Models.Config
+{
+    public class ConnectionString
+    {
+        public const string ConnectionStr =
+            "Server =(localdb)\\MSSQLLocalDB; Database=StudentSystem;Integrated Security = True; TrustServerCertificate=True;";
+    }
+}
---
+++ EntityFramework/Entity Relations/01.StudentSystem/P01_StudentSystem/Data/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+            StudentsCourses = new HashSet<StudentCourse>();
+            Homeworks = new HashSet<Homework>();
+        }
+
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        [MaxLength(ValidationConstants.NameMaxLength)]
+        [Unicode(true)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(ValidationConstants.ExactPhoneNumberLength)]
+        [Unicode(false)]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        public DateTime RegisteredOn { get; set; }
+        public DateTime? Birthday { get; set; }
+
+        public ICollection<StudentCourse> StudentsCourses { get; set; }
+
+        public ICollection<Homework> Homeworks { get; set; }
+
+    }
+}
