19b79f82d3b539ec2b482dd47177460a8680e120
Miroslav <77319935+NitroDaxs@users.noreply.github.com>
2023-11-02 12:31:27 +0200

    Delete EntityFramework/Entity Relations directory

--- EntityFramework/Entity Relations/01. Student System/01.StudentSystem/P01_StudentSystem/Data/Models/Config/ConnectionString.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace P01_StudentSystem.Data.Models.Config
-{
-    public class ConnectionString
-    {
-        public const string ConnectionStr =
-            "Server =(localdb)\\MSSQLLocalDB; Database=StudentSystem;Integrated Security = True; TrustServerCertificate=True;";
-    }
-}
